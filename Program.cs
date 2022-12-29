75556774fe66f381511301bee7c4de372e2e9ea6
ghassen briki <ghassenbriki1998@outlook.com>
2022-12-29 13:12:45 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+internal class Program
+{
+ 
+   static void Main(string[] args)
+    {
+        var c = new Test();
+
+        try
+        {
+            c.OpenFile("example.txt");
+        }
+
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        finally
+        {
+            c.Dispose();
+        }
+         
+           
+    }
+
+}
+
+    internal  class Test : IDisposable
+    {
+        private StreamContent? streamContent = null;
+
+        public void OpenFile(string filename)
+        {
+            streamContent= new StreamContent(File.Open("example.txt", FileMode.Open)); // unmaged Ressources
+        }
+        public void Dispose()
+        {
+            streamContent?.Dispose(); // Here we dispose of the unmanaged resource
+            GC.SuppressFinalize(this); // Preventing a redundant garbage collector finalize call
+        }
+    }
+
+
+
+
